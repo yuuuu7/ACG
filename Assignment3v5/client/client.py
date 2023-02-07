9c7d9e5f17f5cab3fe78fde2a29c3317fed8ee62
Yu <particlezgt@gmail.com>
2023-02-08 02:19:27 +0800

    nothing again bro fk

--- Assignment3v5/client/client.py
+++ Assignment3v5/client/client.py
@@ -3,14 +3,18 @@
 #------------------------------------------------------------------------------------------
 #!/usr/bin/env python3
 # Please starts the tcp server first before running this client
- 
+
 import datetime
 import sys # handle system error
 import socket
 import time
 import os
-from Cryptodome.Cipher import AES
-from Cryptodome.Util import Padding
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding, serialization, hashes
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from Cryptodome.PublicKey import RSA
+from Cryptodome.Cipher import PKCS1_OAEP
+
 
 global host, port
 host = socket.gethostname()
@@ -19,16 +23,33 @@ cmd_GET_MENU = b"GET_MENU"
 cmd_END_DAY = b"CLOSING"
 menu_file = "menu.csv"
 return_file = "day_end.csv"
-key = os.urandom(32)
+key = os.urandom(32) # generate a 32-byte key for encryption
 
-# function to encrypt the data using AES
 def encrypt_data(data, key):
-    cipher = AES.new(key, AES.MODE_CBC, os.urandom(AES.block_size))
-    data = Padding.pad(data, AES.block_size)
-    ciphertext = cipher.encrypt(data)
+    backend = default_backend()
+    cipher = Cipher(algorithms.AES(key), modes.CBC(os.urandom(16)), backend=backend)
+    encryptor = cipher.encryptor()
+    padder = padding.PKCS7(128).padder()
+    data = padder.update(data) + padder.finalize()
+    ciphertext = encryptor.update(data) + encryptor.finalize()
     return ciphertext
 
-# Request for today's menu from the server
+def initialize_keys(password: str): 
+    try: 
+        with open("private.pem", "rb") as f: 
+            private_key = RSA.import_key(f.read(), passphrase=password.encode()) 
+            private_enc = PKCS1_OAEP.new(private_key) 
+    except: 
+        print(f"Authenticity of private key could not be verified. Ensure that the key is correct.") 
+        sys.exit()
+
+key = RSA.generate(2048) 
+passphrase = b"server" 
+private_key = key.export_key(pkcs=8, protection="scryptAndAES128-CBC", passphrase=passphrase) 
+with open("private.pem", "wb") as f: 
+    f.write(private_key) 
+public_key = key.publickey().export_key()
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as my_socket:
     my_socket.connect((host, port))
     my_socket.sendall(cmd_GET_MENU )
@@ -38,26 +59,26 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as my_socket:
     menu_file.close()
     my_socket.close()
     print('Menu today received from server')
+    my_socket.close()
 
-# Sending end-of-day sales report to the server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as my_socket:
     my_socket.connect((host, port))
     my_socket.sendall(cmd_END_DAY)
     try:
-        out_file = open(return_file,"rb")
+     out_file = open(return_file,"rb")
     except:
         print("file not found : " + return_file)
         sys.exit(0)
-    file_bytes = out_file.read(1024)
-    sent_bytes=b''
+        file_bytes = out_file.read(1024)
+        sent_bytes=b''
     while file_bytes != b'':
         encrypted_data = encrypt_data(file_bytes, key)
         my_socket.send(encrypted_data)
         sent_bytes+=file_bytes
         file_bytes = out_file.read(1024) # read next block from file
-    out_file.close()
-    my_socket.close()
-    print('Sale of the day sent to server')
-
+        out_file.close()
+        my_socket.close()
+        print('Sale of the day sent to server')
+        my_socket.close()
 
 
--- Assignment3v5/server/server.py
+++ Assignment3v5/server/server.py
@@ -9,7 +9,14 @@ import traceback        # for print_exc function
 import time             # for delay purpose
 import Cryptodome.Cipher.AES as AES
 import hashlib
-
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.x509.oid import NameOID 
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+import ssl
 import os
 global host, port
 
@@ -22,7 +29,6 @@ host = socket.gethostname() # get the hostname or ip address
 port = 8888                 # The port used by the server
 key = os.urandom(32)
 key2 = os.urandom(32)
-
 
 def encrypt_aes(key, plaintext):
     cipher = AES.new(key, AES.MODE_EAX)
@@ -46,14 +52,14 @@ def process_connection( conn , ip_addr, MAX_BUFFER_SIZE):
                     read_bytes = src_file.read(MAX_BUFFER_SIZE)
                     if read_bytes == b'':
                         break
-                    openedFile = open(default_menu)
+                    '''openedFile = open(default_menu)
                     readFile = openedFile.read()
                     menu_hashed = hashlib.sha256(readFile.encode())
-                    encrypted_hash_menu = encrypt_aes(key2, menu_hashed)
+                    encrypted_hash_menu = encrypt_aes(key2, menu_hashed)'''
                     
                     encrypted_read_bytes = encrypt_aes(key, read_bytes)
                     conn.send(encrypted_read_bytes)
-                    conn.send(encrypted_hash_menu)
+                    #conn.send(encrypted_hash_menu)
                 src_file.close()
                 print("Processed SENDING menu") 
                 return
@@ -88,6 +94,14 @@ def client_thread(conn, ip, port, MAX_BUFFER_SIZE = 4096):
 
 def start_server():
     global host, port
+    # Load the self-signed certificate and private key
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    # Set the verify mode to CERT_REQUIRED to enforce certificate verification
+    context.verify_mode = ssl.CERT_REQUIRED
+    # Load the certificate and private key
+    context.load_cert_chain(certfile="server_cert.crt", keyfile="private_key.pem")
+    # Load the CA certificates
+    context.load_verify_locations(cafile='./server_cert.crt')
     # Here we made a socket instance and passed it two parameters. AF_INET and SOCK_STREAM. 
     soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # this is for easy starting/killing the app
@@ -109,11 +123,23 @@ def start_server():
 
     # this will make an infinite loop needed for 
     # not reseting server for every client
+    # this will make an infinite loop needed for 
+    # not reseting server for every client
     try:
         while True:
             conn, addr = soc.accept()
+            # Upgrade the connection to use SSL/TLS
+            conn = context.wrap_socket(conn, server_side=True)
+            # Check if the certificate presented by the client is valid
+            try:
+                ssl.match_hostname(conn.getpeercert(), host)
+            except ssl.CertificateError:
+                print('Invalid certificate presented by the client')
+                conn.close()
+                continue
             # assign ip and port
             ip, port = str(addr[0]), str(addr[1])
+            print("SUCCESSFUL CERT AUTHENTICATION")
             print('Accepting connection from ' + ip + ':' + port)
             try:
                 Thread(target=client_thread, args=(conn, ip, port)).start()
@@ -125,4 +151,53 @@ def start_server():
     soc.close()
     return
 
-start_server()
+'''private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048,
+    backend=default_backend()
+)
+
+private_key_pem = private_key.private_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PrivateFormat.PKCS8,
+    encryption_algorithm=serialization.NoEncryption()
+)
+
+public_key = private_key.public_key()
+public_key_pem = public_key.public_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo
+)
+
+subject = issuer = x509.Name([
+    x509.NameAttribute(NameOID.COUNTRY_NAME, u"SG"),
+    x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u"SP"),
+    x509.NameAttribute(NameOID.LOCALITY_NAME, u"ACG"),
+    x509.NameAttribute(NameOID.ORGANIZATION_NAME, u"ACG Demo"),
+    x509.NameAttribute(NameOID.COMMON_NAME, u"Client 2205513"),
+])
+
+cert = x509.CertificateBuilder().subject_name(
+    subject
+).issuer_name(
+    issuer
+).public_key(
+    public_key
+).serial_number(
+    x509.random_serial_number()
+).not_valid_before(
+    datetime.datetime.utcnow()
+).not_valid_after(
+    # Our certificate will be valid for 10 years
+    datetime.datetime.utcnow() + datetime.timedelta(days=3650)
+).sign(private_key, hashes.SHA256(), default_backend())
+
+cert_pem = cert.public_bytes(serialization.Encoding.PEM)
+
+with open("private_key.pem", "wb") as f:
+    f.write(private_key_pem)    
+
+with open("server_cert.crt", "wb") as cert_file: 
+  cert_file.write(cert_pem)
+
+start_server()'''
